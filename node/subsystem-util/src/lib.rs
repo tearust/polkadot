737daa16ed3a1eed6e0b8fea33c9b417755f97df
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-01-06 16:27:43 +0100

    break out validator-discovery into its own crate

--- node/subsystem-util/src/lib.rs
+++ node/subsystem-util/src/lib.rs
@@ -49,8 +49,6 @@ use std::{
 use streamunordered::{StreamUnordered, StreamYield};
 use thiserror::Error;
 
-pub mod validator_discovery;
-
 /// These reexports are required so that external crates can use the `delegated_subsystem` macro properly.
 pub mod reexports {
 	pub use sp_core::traits::SpawnNamed;
